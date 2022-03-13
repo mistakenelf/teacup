a44590a893c47236ae21a2d7967655c363a45c0d
knipferrc <knipferrc@gmail.com>
2022-03-13 09:12:23 -0400

    fix: blink input on init

--- filetree/init.go
+++ filetree/init.go
@@ -3,11 +3,20 @@
 package filetree
 
 import (
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/knipferrc/teacup/dirfs"
 )
 
 // Init initializes the filetree with files from the current directory.
 func (b Bubble) Init() tea.Cmd {
-	return getDirectoryListingCmd(dirfs.CurrentDirectory, true)
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+
+	cmd = getDirectoryListingCmd(dirfs.CurrentDirectory, true)
+	cmds = append(cmds, cmd, textinput.Blink)
+
+	return tea.Batch(cmds...)
 }
