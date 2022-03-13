dd87bd5b883a4a756819ac3e18f4adeebbf15f8a
knipferrc <knipferrc@gmail.com>
2022-03-13 09:25:54 -0400

    chore: create methods file

---
+++ filetree/methods.go
@@ -0,0 +1,28 @@
+// Package filetree implements a filetree bubble which can be used
+// to navigate the filesystem and perform actions on files and directories.
+package filetree
+
+import "github.com/charmbracelet/lipgloss"
+
+// SetSize sets the size of the filetree.
+func (b *Bubble) SetSize(width, height int) {
+	horizontal, vertical := bubbleStyle.GetFrameSize()
+
+	b.list.Styles.StatusBar.Width(width - horizontal)
+	b.list.SetSize(width-horizontal, height-vertical-lipgloss.Height(b.input.View())-inputStyle.GetVerticalPadding())
+}
+
+// SetBorderColor sets the color of the border.
+func (b *Bubble) SetBorderColor(color lipgloss.AdaptiveColor) {
+	bubbleStyle = bubbleStyle.Copy().BorderForeground(color)
+}
+
+// GetSelectedItem returns the currently selected item in the tree.
+func (b Bubble) GetSelectedItem() Item {
+	selectedDir, ok := b.list.SelectedItem().(Item)
+	if ok {
+		return selectedDir
+	}
+
+	return Item{}
+}
--- filetree/update.go
+++ filetree/update.go
@@ -8,32 +8,8 @@ import (
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/knipferrc/teacup/dirfs"
 )
-
-// SetSize sets the size of the filetree.
-func (b *Bubble) SetSize(width, height int) {
-	horizontal, vertical := bubbleStyle.GetFrameSize()
-
-	b.list.Styles.StatusBar.Width(width - horizontal)
-	b.list.SetSize(width-horizontal, height-vertical-lipgloss.Height(b.input.View())-inputStyle.GetVerticalPadding())
-}
-
-// SetBorderColor sets the color of the border.
-func (b *Bubble) SetBorderColor(color lipgloss.AdaptiveColor) {
-	bubbleStyle = bubbleStyle.Copy().BorderForeground(color)
-}
-
-// GetSelectedItem returns the currently selected item in the tree.
-func (b Bubble) GetSelectedItem() Item {
-	selectedDir, ok := b.list.SelectedItem().(Item)
-	if ok {
-		return selectedDir
-	}
-
-	return Item{}
-}
 
 // Update handles updating the filetree.
 func (b Bubble) Update(msg tea.Msg) (Bubble, tea.Cmd) {
