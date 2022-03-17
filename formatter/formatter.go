e3cd7d1b5372b5955def9a1523184b9fca3b18ab
knipferrc <knipferrc@gmail.com>
2022-03-17 16:04:43 -0400

    chore: move to filetree

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -2,40 +2,9 @@ package formatter
 
 import (
 	"bytes"
-	"fmt"
 
 	"github.com/ledongthuc/pdf"
 )
-
-// ConvertBytesToSizeString converts a byte count to a human readable string.
-func ConvertBytesToSizeString(size int64) string {
-	if size < 1000 {
-		return fmt.Sprintf("%dB", size)
-	}
-
-	suffix := []string{
-		"K", // kilo
-		"M", // mega
-		"G", // giga
-		"T", // tera
-		"P", // peta
-		"E", // exa
-		"Z", // zeta
-		"Y", // yotta
-	}
-
-	curr := float64(size) / 1000
-	for _, s := range suffix {
-		if curr < 10 {
-			return fmt.Sprintf("%.1f%s", curr-0.0499, s)
-		} else if curr < 1000 {
-			return fmt.Sprintf("%d%s", int(curr), s)
-		}
-		curr /= 1000
-	}
-
-	return ""
-}
 
 // ReadPdf reads a PDF file given a name.
 func ReadPdf(name string) (string, error) {
