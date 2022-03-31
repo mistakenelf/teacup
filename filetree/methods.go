d1624382db345ee61a9afa256f62a68dd35f2325
knipferrc <knipferrc@gmail.com>
2022-03-31 16:40:29 -0400

    fix: fix tree sizing

--- filetree/methods.go
+++ filetree/methods.go
@@ -43,7 +43,7 @@ func (b *Bubble) SetSize(width, height int) {
 	horizontal, vertical := bubbleStyle.GetFrameSize()
 
 	b.list.Styles.StatusBar.Width(width - horizontal)
-	b.list.SetSize(width-horizontal, height-vertical-lipgloss.Height(b.input.View())-inputStyle.GetVerticalPadding())
+	b.list.SetSize(width-horizontal-vertical, height-vertical-lipgloss.Height(b.input.View())-inputStyle.GetVerticalPadding())
 }
 
 // SetBorderColor sets the color of the border.
