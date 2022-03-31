fb10243d2e4a8c2584e08661fe8a251a73d9b0a2
knipferrc <knipferrc@gmail.com>
2022-03-31 16:40:52 -0400

    feat: add statusbar bubble and example

---
+++ examples/statusbar/statusbar.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/knipferrc/teacup/statusbar"
+)
+
+// Bubble represents the properties of the UI.
+type Bubble struct {
+	statusbar statusbar.Bubble
+	height    int
+}
+
+// New creates a new instance of the UI.
+func New() Bubble {
+
+	return Bubble{
+		statusbar: statusbar.Bubble{},
+	}
+}
+
+// Init intializes the UI.
+func (b Bubble) Init() tea.Cmd {
+	return nil
+}
+
+// Update handles all UI interactions.
+func (b Bubble) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		cmds []tea.Cmd
+	)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		b.height = msg.Height
+		b.statusbar.SetSize(msg.Width)
+		b.statusbar.SetContent("test.txt", "~/.config/nvim", "1/23", "SB")
+
+		return b, nil
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc", "q":
+			cmds = append(cmds, tea.Quit)
+		}
+	}
+
+	return b, tea.Batch(cmds...)
+}
+
+// View returns a string representation of the UI.
+func (b Bubble) View() string {
+	return lipgloss.JoinVertical(
+		lipgloss.Top,
+		lipgloss.NewStyle().Height(b.height-statusbar.Height).Render("Content"),
+		b.statusbar.View(),
+	)
+}
+
+func main() {
+	b := New()
+	p := tea.NewProgram(b, tea.WithAltScreen())
+
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
--- statusbar/statusbar.go
+++ statusbar/statusbar.go
@@ -8,7 +8,7 @@ import (
 	"github.com/muesli/reflow/truncate"
 )
 
-const statusBarHeight = 1
+const Height = 1
 
 // Bubble represents the properties of the statusbar.
 type Bubble struct {
@@ -21,7 +21,7 @@ type Bubble struct {
 }
 
 // SetSize sets the width of the statusbar.
-func (b *Bubble) SetSize(width) {
+func (b *Bubble) SetSize(width int) {
 	b.Width = width
 }
 
@@ -35,37 +35,44 @@ func (b Bubble) Update(msg tea.Msg) (Bubble, tea.Cmd) {
 	return b, nil
 }
 
-
 // SetContent sets the content of the statusbar.
 func (b *Bubble) SetContent(firstColumn, secondColumn, thirdColumn, fourthColumn string) {
-  b.FirstColumn = firstColumn
-  b.SecondColumn = secondColumn
-  b.ThirdColumn = thirdColumn
-  b.FourthColumn = fourthColumn
+	b.FirstColumn = firstColumn
+	b.SecondColumn = secondColumn
+	b.ThirdColumn = thirdColumn
+	b.FourthColumn = fourthColumn
 }
 
 func (b Bubble) View() string {
-  width := lipgloss.Width
+	width := lipgloss.Width
 
 	firstColumn := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Background(lipgloss.Color("#F25D94")).
 		Padding(0, 1).
-		Height(statusBarHeight).
+		Height(Height).
 		Render(truncate.StringWithTail(b.FirstColumn, 30, "..."))
 
 	thirdColumn := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Background(lipgloss.Color("#A550DF")).
 		Align(lipgloss.Right).
 		Padding(0, 1).
-		Height(statusBarHeight).
+		Height(Height).
 		Render(b.ThirdColumn)
 
 	fourthColumn := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Background(lipgloss.Color("#6124DF")).
 		Padding(0, 1).
-		Height(statusBarHeight).
+		Height(Height).
 		Render(b.FourthColumn)
 
 	secondColumn := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#ffffff")).
+		Background(lipgloss.Color("#3c3836")).
 		Padding(0, 1).
-		Height(statusBarHeight).
+		Height(Height).
 		Width(b.Width - width(firstColumn) - width(thirdColumn) - width(fourthColumn)).
 		Render(truncate.StringWithTail(
 			b.SecondColumn,
